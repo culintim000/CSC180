20bcccb2b68ff6c8626108a0a1a62f60ed729a5a
culintim000 <cultim000@gmail.com>
2021-07-06 09:47:10 -0600

    testing the git push

---
+++ JUnitTest/src/main/java/TheStart.java
@@ -0,0 +1,18 @@
+
+public class TheStart {
+
+	public static void main(String[] args) {
+
+		double intReturn;
+		
+		
+		System.out.println("Add: " + Calculator.add(12.4, 54.2));
+		System.out.println("Subtract: " + Calculator.subtract(12.4, 54.2));
+		System.out.println("Multiply: " + Calculator.multiply(12.4, 54.2));
+		System.out.println("Devide: " + Calculator.devide(12.4, 54.2));
+		
+		System.out.println("Printing Something to test");
+
+	}
+
+}
